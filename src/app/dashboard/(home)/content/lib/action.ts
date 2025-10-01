b95f98fc8c831c09009364dea65056228414c9f6
Jonathan Gunawan <jgunawan3005@gmail.com>
2025-10-01 13:45:12 +0700

    refactor(data): Separate server and client data fetching functions

--- src/app/dashboard/(home)/content/lib/action.ts
+++ src/app/dashboard/(home)/content/lib/action.ts
@@ -1,7 +1,5 @@
-import axiosInstance from "@lib/axios";
+import axiosInstance from "@/lib/axios";
 import { Category } from "@/model/Category";
-import { Content } from "@/model/Content";
-import { cookies } from "next/headers";
 
 /**
  * CREATE content
@@ -60,29 +58,6 @@ export const uploadImage = async (file: File): Promise<{ url: string }> => {
         return { url: res.data.data.url_image };
     } catch (err) {
         console.error("Upload error:", err);
-        throw err;
-    }
-};
-
-
-/**
- * GET content by id
- */
-export const getContentById = async (id: number): Promise<Content> => {
-    const cookieStore = cookies();
-    const token = cookieStore.get('X-API-TOKEN')?.value;
-    if (!token) {
-        throw new Error("Unauthorized: No session cookie found");
-    }
-    try {
-        const res = await axiosInstance.get(`/admin/contents/${id}`, {
-            headers: {
-                'Cookie': `X-API-TOKEN=${token}`
-            }
-        });
-        return res.data.data;
-    } catch (err) {
-        console.error("Server-side fetch error:", err);
         throw err;
     }
 };
---
+++ src/app/dashboard/(home)/content/lib/data.ts
@@ -0,0 +1,26 @@
+import axiosInstance from "@/lib/axios";
+import { Content } from "@/model/Content";
+import { cookies } from "next/headers";
+
+/**
+ * GET content by id (For SERVER-SIDE fetching)
+ */
+export const getContentById = async (id: number): Promise<Content> => {
+    const cookieStore = cookies();
+    const token = cookieStore.get('X-API-TOKEN')?.value;
+
+    if (!token) {
+        throw new Error("Unauthorized: No session cookie found");
+    }
+
+    try {
+        const res = await axiosInstance.get(`/admin/contents/${id}`, {
+            headers: {
+                'Cookie': `X-API-TOKEN=${token}`
+            }
+        });
+        return res.data.data;
+    } catch (err) {
+        throw err;
+    }
+};
